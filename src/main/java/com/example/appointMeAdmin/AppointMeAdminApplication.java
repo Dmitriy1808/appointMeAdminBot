2e61fc764b9ea86827d1000c1cd0c81cbfda4cac
Dmitry Golcev <dmitrij.99@yandex.ru>
2023-09-19 16:08:31 +0000

    add project carcase

---
+++ src/main/java/com/example/appointMeAdmin/AppointMeAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.appointMeAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointMeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointMeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appointMeAdmin/AppointMeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appointMeAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointMeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
